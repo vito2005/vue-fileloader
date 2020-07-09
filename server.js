0c2d2a985cf3a2ccaf9982067b1c354bc46b0eab
abuki <macbook@MacBook-Pro-409.local>
2020-07-09 14:27:10 +0300

    find bug

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const app = express()
 
 // app.use(express.static(path.join(__dirname, '/dist/')))
 // app.get('/.*/', (req, res) => res.sendFile(path.join(__dirname, '/dist/index.html')))
-app.get('/.*/', (req, res) => res.end('hello'))
+app.get('/', (req, res) => res.end('hello'))
 
 app.listen(5000, () => {
   console.log('server has been started')
